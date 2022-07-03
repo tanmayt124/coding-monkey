98545ae30e775473b1f021e9812385b37e5d7855
Tanmay Thakur <tanmaytha9821@gmail.com>
2022-07-03 15:33:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import Aboutme from './components/Aboutme_/Aboutme';
 import Services from './components/Services_/Services';
 import Contactme from './components/Contactme_/Contactme';
 import Footer from './components/Footer_/Footer';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPdLuSnv3NxbZvb7QQLyxI4QPlUwXCsl0",
+    authDomain: "codingmonkey-965a5.firebaseapp.com",
+    projectId: "codingmonkey-965a5",
+    storageBucket: "codingmonkey-965a5.appspot.com",
+    messagingSenderId: "63785008302",
+    appId: "1:63785008302:web:143b33e89ef1833e56813f",
+    measurementId: "G-ZLSQP1PLCJ"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App () {
     return(
